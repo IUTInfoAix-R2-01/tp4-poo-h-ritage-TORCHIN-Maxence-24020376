2828ba14de8c64c63018340606be1e362722520e
Maxence TORCHIN <maxence.torchin@etu.univ-amu.fr>
2025-02-14 09:24:19 +0100

    Initialisation des getters et des setters

---
+++ src/cycles/Velo.java
@@ -0,0 +1,41 @@
+package cycles;
+
+import java.util.Random;
+
+public class Velo {
+	private double braquet;
+	private double diamRoue;
+	private Random genAlea;
+	
+	public Random getGenAlea() {
+		return Random;
+	}
+	
+	public void SetGenAlea(Random genAlea) {
+		
+	}
+	
+	public double getBraquet() {
+		return braquet;
+	}
+	
+	public void setBraquet(double braquet) {
+		
+	}
+	
+	public double getDiamRoue() {
+		return diamRoue;
+	}
+	
+	public void setDiamRoue(double diamRoue) {
+		
+	}
+	
+	public String toString() {
+		
+	}
+	
+	public double getPuissance(double frequenceCoupsDePedale) {
+		
+	}
+}
