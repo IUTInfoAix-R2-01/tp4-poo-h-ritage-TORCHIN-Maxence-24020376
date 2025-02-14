bf4bb61a3e0923dbc7efb1e5eb6c5faf2b0ad175
Maxence TORCHIN <maxence.torchin@etu.univ-amu.fr>
2025-02-14 09:37:21 +0100

    Remplissage des fonctions

--- src/cycles/Velo.java
+++ src/cycles/Velo.java
@@ -6,36 +6,36 @@ public class Velo {
 	private double braquet;
 	private double diamRoue;
 	private Random genAlea;
-	
+
 	public Random getGenAlea() {
-		return Random;
+		return genAlea;
 	}
-	
-	public void SetGenAlea(Random genAlea) {
-		
+
+	public void setGenAlea(Random genAlea) {
+		this.genAlea = genAlea;
 	}
-	
+
 	public double getBraquet() {
 		return braquet;
 	}
-	
+
 	public void setBraquet(double braquet) {
-		
+		this.braquet = braquet;
 	}
-	
+
 	public double getDiamRoue() {
 		return diamRoue;
 	}
-	
+
 	public void setDiamRoue(double diamRoue) {
-		
+		this.diamRoue = diamRoue;
 	}
 	
 	public String toString() {
-		
+		return "velo[braquet=" + braquet + ", diamRoue" + diamRoue();
 	}
 	
 	public double getPuissance(double frequenceCoupsDePedale) {
-		
-	}
+		return frequenceCoupsDePedale * braquet * diamRoue *genAlea.nextDouble();	}
+
 }
