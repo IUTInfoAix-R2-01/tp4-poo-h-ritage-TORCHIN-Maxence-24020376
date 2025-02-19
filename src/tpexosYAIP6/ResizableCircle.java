df384791cbcfba396a16103c2c7edad50713a193
Maxence TORCHIN <maxence.torchin@etu.univ-amu.fr>
2025-02-20 00:51:37 +0100

    Ajout d'un test

--- src/tpexosYAIP6/ResizableCircle.java
+++ src/tpexosYAIP6/ResizableCircle.java
@@ -14,4 +14,15 @@ public class ResizableCircle extends Circle implements Resizable {
 	public void resize(int percent) {
 		System.out.println(radius *= percent/100.0);
 	}
+	
+	public static void main(String[] args) {
+        ResizableCircle c1 = new ResizableCircle(10.0);
+        System.out.println("Avant resize : " + c1);
+
+        c1.resize(50);
+        System.out.println("Après réduction de 50% : " + c1);
+
+        c1.resize(200);
+        System.out.println("Après augmentation de 200% : " + c1);
+    }
 }
